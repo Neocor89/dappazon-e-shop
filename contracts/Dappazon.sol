f8a46838b7b0c3cc03225cced60df254a926eef4
Ben <ncor89b@gmail.com>
2023-01-29 00:22:17 +0100

    Implementation product buying logic in smart contract

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -27,6 +27,19 @@ contract Dappazon {
 
   mapping(uint256 => Item) public items;
   //: mapping("Key" = dataType of Key => "value" = Struct Item
+
+  // Event Step 1 Define the "event" with tracked values
+  //! WARNING FOR POTENTIAL ERROR "QUANTITY" IN THIS LINE  🚨
+  event List(string name, uint256 cost, uint256 quantity);
+
+
+  // Modifier Step 1 Define
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+   //: Check condition and return true or false
+  }
+
   constructor() {
     owner = msg.sender;
   }
@@ -44,8 +57,8 @@ contract Dappazon {
     uint256 _cost, 
     uint256 _rating, 
     uint256 _stock
-  ) public {
-
+  ) public onlyOwner {
+    // Modifier Step 2 Apply to the function
 
   // Step 3  
   // Create Item Struct
@@ -56,13 +69,31 @@ contract Dappazon {
   //: item = Assign and Ceate a new variable
   //: Item() = Create a new Item struct 
 
+
   // Step 4 
   // Save Item Struct on the Blockchain
   items[_id] = item;
+
+
+  // Emit EVENT on Blockchain
+  emit List(_name, _cost, _stock);
+
+  /*
+  : In Solidity is better emit event in a Function
+  : Using "event" Keyword  
+  : More tracability = when the function is called on a Blockchain
+  : It's possible to recive an alert  
+  */
   }
 
 
   // Buy Products
+  function buy(uint256 _id) public payable {
+    // Receive Crypto
+
+    // Create Order
+
+  }
 
   //: WithDraw funds
 
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -4,6 +4,16 @@ const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
 };
 
+//: Global constants for testing List if Items
+const ID = 1;
+const NAME = "Shoes";
+const CATEGORY = "Clothing";
+const IMAGE =
+  "https://ipfs.io/ipfs/QmTYEboq8raiBs7GTUg2yLXB3PMz6HuBNgNfSZBx5Msztg/shoes.jpg";
+const COST = tokens(1);
+const RATING = 4;
+const STOCK = 5;
+
 describe("Dappazon", () => {
   let dappazon;
   let deployer, buyer;
@@ -29,14 +39,47 @@ describe("Dappazon", () => {
     beforeEach(async () => {
       transaction = await dappazon
         .connect(deployer)
-        .list(1, "Shoes", "Clothing", "IMAGE", 1, 4, 5);
+        .list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
 
       await transaction.wait();
     });
 
     it("Returns item attributes", async () => {
-      const item = await dappazon.items(1);
-      expect(item.id).to.equal(1);
+      const item = await dappazon.items(ID);
+
+      expect(item.id).to.equal(ID);
+      expect(item.name).to.equal(NAME);
+      expect(item.category).to.equal(CATEGORY);
+      expect(item.image).to.equal(IMAGE);
+      expect(item.cost).to.equal(COST);
+      expect(item.rating).to.equal(RATING);
+      expect(item.stock).to.equal(STOCK);
+    });
+
+    it("Emits List Event", () => {
+      expect(transaction).to.emit(dappazon, "list");
+    });
+  });
+
+  describe("Buying", () => {
+    let transaction;
+
+    beforeEach(async () => {
+      // List an Item
+      transaction = await dappazon
+        .connect(deployer)
+        .list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
+      await transaction.wait();
+
+      //Buy an Item
+      transaction = await dappazon.connect(buyer).buy(ID, { value: COST });
+    });
+
+    // TODO --> RESTART HERE 🚀👷
+
+    it("Updates the Contract balance", async () => {
+      const result = await ethers.provider.getBalance(dappazon.address);
+      expect(result).to.emit(COST);
     });
   });
 });
