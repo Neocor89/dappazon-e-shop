d438014d432f8ea04a6d9c179cba5932552a16b6
Ben <ncor89b@gmail.com>
2023-01-29 19:18:16 +0100

    feat: implement orders and buy event

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -16,6 +16,13 @@ contract Dappazon {
     uint256 stock;
   }
 
+  // Step1 -> Create Struct for Order
+  struct Order {
+    uint256 time;
+    Item item;
+    //: Item item = struct Item nested
+  }
+
 
  /*
   + Mapping 
@@ -28,16 +35,25 @@ contract Dappazon {
   mapping(uint256 => Item) public items;
   //: mapping("Key" = dataType of Key => "value" = Struct Item
 
+  mapping (address => uint256) public orderCount;
+  //: address = key of user create Order
+  //: uint256 = number Order receive
+
+  mapping (address => mapping(uint256 => Order)) public orders;
+  //: mapping for quantity products of the individual Order
+
+  event Buy(address buyer, uint256 orderId, uint256 itemId);
   // Event Step 1 Define the "event" with tracked values
-  //! WARNING FOR POTENTIAL ERROR "QUANTITY" IN THIS LINE  🚨
   event List(string name, uint256 cost, uint256 quantity);
 
 
-  // Modifier Step 1 Define
+  //! WARNING FOR POTENTIAL ERROR "QUANTITY" IN THIS LINE  🚨
+
+
+  // Modifier Step 1 -> Define
   modifier onlyOwner() {
     require(msg.sender == owner);
     _;
-   //: Check condition and return true or false
   }
 
   constructor() {
@@ -58,40 +74,52 @@ contract Dappazon {
     uint256 _rating, 
     uint256 _stock
   ) public onlyOwner {
-    // Modifier Step 2 Apply to the function
-
-  // Step 3  
-  // Create Item Struct
-
-  Item memory item = Item(_id, _name, _category, _image, _cost, _rating,_stock);
-  //: Item = Item struct 
-  //: memory = location of provide this information
-  //: item = Assign and Ceate a new variable
-  //: Item() = Create a new Item struct 
+    // Modifier Step 2 -> Apply to the function
 
 
-  // Step 4 
-  // Save Item Struct on the Blockchain
-  items[_id] = item;
+    // Step 3  
+    // Create Item Struct
+    Item memory item = Item(_id, _name, _category, _image, _cost, _rating,_stock);
+    //: Item = Item struct 
+    //: memory = location of provide this information
+    //: item = Assign and Ceate a new variable
+    //: Item() = Create a new Item struct 
 
 
-  // Emit EVENT on Blockchain
-  emit List(_name, _cost, _stock);
+    // Step 4 
+    // Save Item Struct on the Blockchain
+    items[_id] = item;
 
-  /*
-  : In Solidity is better emit event in a Function
-  : Using "event" Keyword  
-  : More tracability = when the function is called on a Blockchain
-  : It's possible to recive an alert  
-  */
+
+    // Emit EVENT on Blockchain
+    emit List(_name, _cost, _stock);
+    /*
+    : In Solidity is better emit event in a Function
+    : Using "event" Keyword  
+    : More tracability = when the function is called on a Blockchain
+    : It's possible to recive an alert  
+    */
+   
   }
 
 
   // Buy Products
   function buy(uint256 _id) public payable {
-    // Receive Crypto
+    // Fetch Item
+    Item memory item = items[_id];
 
-    // Create Order
+    // Step2 -> Create Order Struct
+    Order memory order = Order(block.timestamp, item);
+
+    //: Add an Order for User
+    orderCount[msg.sender]++;
+    orders[msg.sender][orderCount[msg.sender]] = order;
+
+    // Substrack item stock
+    items[_id].stock = item.stock -1;
+
+    // Emit Event
+    emit Buy(msg.sender, orderCount[msg.sender], item.id);
 
   }
 
