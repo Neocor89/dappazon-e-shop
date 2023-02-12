80be9267e25edf0c7f2ee6a310447a45dbf19c24
Ben <ncor89b@gmail.com>
2023-02-12 22:24:40 +0100

    buid: recovery and add smart contract products

--- src/App.js
+++ src/App.js
@@ -13,37 +13,87 @@ import Dappazon from "./abis/Dappazon.json";
 import config from "./config.json";
 
 function App() {
-  //: useState return an Array
-  const [account, setAccount] = useState(null);
   const [provider, setProvider] = useState(null);
+  const [dappazon, setDappazon] = useState(null);
 
-  const loadBlockChainData = async () => {
-    //: Etherjs provider -> Transform App in Blockchain DApp
-    //: Connect to the blockchain React App
+  const [account, setAccount] = useState(null);
+
+  const [electronics, setElectronics] = useState(null);
+  const [clothing, setClothing] = useState(null);
+  const [toys, setToys] = useState(null);
+
+  const [item, setItem] = useState({});
+  const [toggle, setToggle] = useState(false);
+
+  const togglePop = (item) => {
+    setItem(item);
+    toggle ? setToggle(false) : setToggle(true);
+  };
+
+  const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     setProvider(provider);
-
     const network = await provider.getNetwork();
     console.log(network);
 
-    // Connect to SMart Contracts
     const dappazon = new ethers.Contract(
-      "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+      config[network.chainId].dappazon.address,
       Dappazon,
       provider
     );
-    // Load Products
-    // Fetch Products
+    setDappazon(dappazon);
+
+    const items = [];
+
+    for (var i = 0; i < 9; i++) {
+      const item = await dappazon.items(i + 1);
+      items.push(item);
+    }
+
+    const electronics = items.filter((item) => item.category === "electronics");
+    const clothing = items.filter((item) => item.category === "clothing");
+    const toys = items.filter((item) => item.category === "toys");
+
+    setElectronics(electronics);
+    setClothing(clothing);
+    setToys(toys);
   };
 
   useEffect(() => {
-    loadBlockChainData();
+    loadBlockchainData();
   }, []);
 
   return (
     <div>
       <Navigation account={account} setAccount={setAccount} />
-      <h2>Dapp azon</h2>
+
+      <h2>Dappazon Best Sellers</h2>
+
+      {electronics && clothing && toys && (
+        <>
+          <Section
+            title={"Clothing & Jewelry"}
+            items={clothing}
+            togglePop={togglePop}
+          />
+          <Section
+            title={"Electronics & Gadgets"}
+            items={electronics}
+            togglePop={togglePop}
+          />
+          <Section title={"Toys & Gaming"} items={toys} togglePop={togglePop} />
+        </>
+      )}
+
+      {toggle && (
+        <Product
+          item={item}
+          provider={provider}
+          account={account}
+          dappazon={dappazon}
+          togglePop={togglePop}
+        />
+      )}
     </div>
   );
 }
